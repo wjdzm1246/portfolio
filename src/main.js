965606a4ea3ed6d8ddc44fc67caba3aad4addf84
wjdzm1246 <115851910+wjdzm1246@users.noreply.github.com>
2023-11-06 14:16:53 -0600

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// when scroll down, apply dark style in header
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    if (window.scrollY > headerHeight) {
+        header.classList.add('header--dark');
+    } else {
+        header.classList.remove('header--dark');
+    }
+})
