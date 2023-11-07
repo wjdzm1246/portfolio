01af682b7803b65519889b11339073899b93d612
wjdzm1246 <115851910+wjdzm1246@users.noreply.github.com>
2023-11-06 22:48:39 -0600

    JS-Home

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
     }
+})
+
+// when scroll home section, make it transprents
+const home = document.querySelector('.home__container') ;
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1-window.scrollY / homeHeight;
 })
