cd59fe0c135e12882fdd92b72b371b74c98813f1
wjdzm1246 <115851910+wjdzm1246@users.noreply.github.com>
2023-11-07 11:34:08 -0600

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1-window.scrollY / homeHeight;
 })
+
+// when scroll down, apply dark style in header
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight/2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+})
