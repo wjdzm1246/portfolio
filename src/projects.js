53d8fdad6466fa7d08009feb77f778c8af6d08c8
wjdzm1246 <115851910+wjdzm1246@users.noreply.github.com>
2023-11-09 20:37:17 -0600

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// flow for project filtering
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+    const filter = event.target.dataset.category;
+    if (filter == null) {
+        return;
+    }
+
+    handleActiveSelection(event.target);
+    filterProjects(filter);
+
+});
+
+function handleActiveSelection(target) {
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+    projectsContainer.classList.add('anim-out');
+    projects.forEach( (project) => {
+        if (filter === 'all' || filter === project.dataset.type) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+
+    });
+    setTimeout(() => {
+        projectsContainer.classList.remove('anim-out');
+    }, 250);
+}
